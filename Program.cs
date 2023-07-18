92eeb2db6304fa147af6c9732751153364198c03
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-18 23:11:51 +0300

    Update Program.cs
    removed class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using lab4ex3;
 
 namespace lab4ex1
 {
@@ -8,7 +9,7 @@ namespace lab4ex1
         static void Main(string[] args)
 
         {
-            //Console.WriteLine("Hello World!");
+            
 
             /*
       • Ex3
@@ -24,8 +25,8 @@ Adaugati autoturismului un set de
 
 
 
-            autoturism Ford = new autoturism("Ford", "CJ03 123", 1600);
-            autoturism Audi = new autoturism("Audi", "CJ01 456", 1800);
+            Car Ford = new Car("Ford", "CJ03 123", 1600);
+            Car Audi = new Car("Audi", "CJ01 456", 1800);
 
             Console.WriteLine($"Primul autoturism:\t{Ford.GetDescription(Ford.GetMarca(), Ford.GetNumarInmatriculare(), Ford.GetCapacitateCilindrica())} ");
             Console.WriteLine($"Al doilea  autoturism:\t{Audi.GetDescription(Ford.GetMarca(), Audi.GetNumarInmatriculare(), Audi.GetCapacitateCilindrica())}");
@@ -65,128 +66,5 @@ Adaugati autoturismului un set de
 
         }
 
-
-
-
-
-        class autoturism
-        {
-
-            private string marca;
-            private string numarInmatriculare;
-            private int capacitateCilindrica;
-            private List<string> pasageri = new List<string>();
-
-
-            /// <summary>
-            /// constructor, initializeaza marca, nr inmatriculare, caoacitate cilindriuca
-            /// </summary>
-            /// <param name="marca"></param>
-            /// <param name="numarInmatriculare"></param>
-            /// <param name="capacitateCilindrica"></param>
-            public autoturism(string marca, string numarInmatriculare, int capacitateCilindrica)
-            {
-
-                this.marca = marca;
-                this.numarInmatriculare = numarInmatriculare;
-                this.capacitateCilindrica = capacitateCilindrica;
-            }
-
-
-            /// <summary>
-            /// returneaza lista de pasageri
-            /// </summary>
-            /// <returns></returns>
-            public List<string> GetPasageri()
-            {
-                return pasageri;
-            }
-
-
-            /// <summary>
-            /// returneaza marca
-            /// </summary>
-            /// <returns></returns>
-            public string GetMarca()
-            {
-                return marca;
-            }
-
-            /// <summary>
-            /// returneaza nr de inmatriculare
-            /// </summary>
-            /// <returns></returns>
-            public string GetNumarInmatriculare()
-            {
-                return numarInmatriculare;
-            }
-
-            /// <summary>
-            /// returneaza capacitatea cilindrica
-            /// </summary>
-            /// <returns></returns>
-            public int GetCapacitateCilindrica()
-            {
-                return capacitateCilindrica;
-            }
-
-            /// <summary>
-            /// genereaza descrierea unui autoturism: marca, nr inmatriculare, capacitate cilindrica, lista de pasageri
-            /// </summary>
-            /// <param name="marca"></param>
-            /// <param name="numarInmatriculare"></param>
-            /// <param name="capacitateCilindrica"></param>
-            /// <returns></returns>
-            public string GetDescription(string marca, string numarInmatriculare, int capacitateCilindrica)
-            {
-                string result = "pasageri: ";
-                //return GetMarca() + ", " + GetNumarInmatriculare() + ", " + GetCapacitateCilindrica();
-                //return $" {GetMarca()}\t{ GetNumarInmatriculare()}\t{GetCapacitateCilindrica()}\t{GetPasageri()}";
-
-                foreach (var pasager in pasageri)
-                {
-                    result = result  + pasager + ", ";
-                }
-
-                result = $" { GetMarca()}\t{ GetNumarInmatriculare()}\t{ GetCapacitateCilindrica()}\t" + result;
-                
-                return result;
-
-            }
-
-            /// <summary>
-            /// adauga un pasager intr-un autoturism
-            /// </summary>
-            /// <param name="pasager"></param>
-            public void AdaugaPasager (string pasager)
-            {
-                pasageri.Add(pasager);
-            }
-
-           
-
-            /// <summary>
-            /// Verifica un pasager adaugat intr-un autoturism, pe bvza de nume si nr de inmatriculare
-            /// </summary>
-            /// <param name="pasager"></param>
-            /// <param name="numarInmatriculare"></param>
-            /// <returns></returns>
-            public bool VerificaPasagerInMasina(string pasager, string numarInmatriculare)
-            {
-
-                bool result = false;
-
-                for (int i = 0; i < pasageri.Count; i++)
-                {
-                    if (pasageri[i] == pasager && this.numarInmatriculare == numarInmatriculare)
-                    {
-                        result = true;
-                    }
-                }
-
-                return result;
-            }
-
-        }
     }
 }
